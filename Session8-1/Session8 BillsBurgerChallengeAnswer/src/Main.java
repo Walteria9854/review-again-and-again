e9652e11f557f3a2aee8c1fe1172a8aeb843c335
Walteria9854 <yukikatayamaaapu@gmail.com>
2026-07-21 22:25:10 +0800

    adding burger information

--- Session8-1/Session8 BillsBurgerChallengeAnswer/src/Main.java
+++ Session8-1/Session8 BillsBurgerChallengeAnswer/src/Main.java
@@ -10,9 +10,12 @@ public class Main {
 //        Item avocado = new Item("Topping", "avocado", 1.50);
 //        avocado.printItem();
 
-        Burger burger = new Burger("regular", 4.00);
-        burger.addToppings("BACON", "CHEESE", "MAYO");
-        burger.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+        MealOrder regulatMeal = new MealOrder();
+        regulatMeal.printItemizedList();
 
     }
 }
--- Session8-1/Session8 BillsBurgerChallengeAnswer/src/MealOrder.java
+++ Session8-1/Session8 BillsBurgerChallengeAnswer/src/MealOrder.java
@@ -4,10 +4,26 @@ public class MealOrder {
   private Item drink;
   private Item side;
 
+  public MealOrder(){
+    this("regular", "Coke", "fries");
+  }
+
   public MealOrder(String burgerTyoe, String drinkType, String sideType){
     this.burger = new Burger(burgerTyoe,4.0);
     this.drink = new Item("drink",drinkType, 1.00);
     this.side = new Item("side", sideType, 1.50);
+  }
+
+  public double getTotalPrice(){
+    return side.getAdjustedPrice() + drink.getAdjustedPrice() + burger.getAdjustedPrice();
+  }
+
+  public void printItemizedList(){
+    burger.printItem();
+    side.printItem();
+    drink.printItem();
+    System.out.println("-".repeat(30));
+    Item.printItem("TOTAL PRICE", getTotalPrice());
   }
 
 
