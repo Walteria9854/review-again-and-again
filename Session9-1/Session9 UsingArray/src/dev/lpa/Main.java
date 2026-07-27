9258ecd7e6f2c39d3e05078df8b13c4a18c6be0f
Walteria9854 <yukikatayamaaapu@gmail.com>
2026-07-27 23:10:10 +0800

    little update

--- Session9-1/Session9 UsingArray/src/dev/lpa/Main.java
+++ Session9-1/Session9 UsingArray/src/dev/lpa/Main.java
@@ -34,10 +34,17 @@ public class Main {
         String[] sArray = {"Able","Jane","Mark","Ralph", "David"};
         Arrays.sort(sArray);
         System.out.println(Arrays.toString(sArray));
-       if(Arrays.binarySearch(sArray, "Jane") >= 0){
+       if(Arrays.binarySearch(sArray, "Mark") >= 0){
            System.out.println("Found Mark in the list");
-
        }
+
+       int[] s1 = {1,2,3,4,5};
+       int[] s2 = {1,2,3,4,5};
+
+       if(Arrays.equals(s1, s2)){
+           System.out.println("Arrays are equal");
+       }
+
 
 
 
